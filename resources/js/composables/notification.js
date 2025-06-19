ef71d4abd5d83f2524f87996bf1945bc6f5ad8e2
kayeeesan <eijansantosdominique13@gmail.com>
2025-06-19 11:52:33 +0800

    pusher changed to reverb

--- resources/js/composables/notification.js
+++ resources/js/composables/notification.js
@@ -15,11 +15,21 @@ export default function useNotifications() {
   });
 
 
+    // const echo = new Echo({
+    //     broadcaster: 'pusher',
+    //     key: '57206333aea283adecc8',
+    //     cluster: 'ap1',
+    //     forceTLS: true,
+    // });
+
+
     const echo = new Echo({
-        broadcaster: 'pusher',
-        key: '57206333aea283adecc8',
-        cluster: 'ap1',
-        forceTLS: true,
+        broadcaster: 'reverb',
+        key: import.meta.env.REVERB_APP_KEY  || '57206333aea283adecc8',
+        wsHost: import.meta.env.REVERB_HOST || window.location.hostname,
+        wsPort: Number(import.meta.env.REVERB_PORT) || 6001,
+        forceTLS: import.meta.env.REVERB_SCHEME === 'https',
+        enabledTransports: ['ws', 'wss'],
     });
 
       echo.channel('public-alerts')
