11af062506c36faa4a6acbae92c5ff9d9f10fb7a
kayeeesan <eijansantosdominique13@gmail.com>
2025-07-24 14:10:30 +0800

    user successfully can send to main haha

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            // 'password' => 'hashed',
         ];
     }
 
