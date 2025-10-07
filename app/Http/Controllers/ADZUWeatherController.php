8d24a4842f56649f3ea39969b4a8b48d3bf38f71
kayeeesan <eijansantosdominique13@gmail.com>
2025-10-07 15:28:27 +0800

    remove verifying

--- app/Http/Controllers/ADZUWeatherController.php
+++ app/Http/Controllers/ADZUWeatherController.php
@@ -9,7 +9,7 @@ class ADZUWeatherController extends Controller
 {
     public function getWeather()
     {
-        $response = Http::get("https://api.weatherlink.com/v1/NoaaExt.json", [
+        $response = Http::withoutVerifying()->get("https://api.weatherlink.com/v1/NoaaExt.json", [
             'user' => '001D0A002457',
             'pass' => 't@b1ngd@gat',
             'apiToken' => '3BFA895305E34127A232DF10508F51E6'
