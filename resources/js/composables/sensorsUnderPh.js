25b6abeddf72595231e3be6dc25b2d50e100d879
kayeeesan <eijansantosdominique13@gmail.com>
2025-11-13 12:00:18 +0800

    pusher added

--- resources/js/composables/sensorsUnderPh.js
+++ resources/js/composables/sensorsUnderPh.js
@@ -17,22 +17,22 @@ export default function useSensorsUnderPh() {
         page: 1,
     });
 
-    //  const echo = new Echo({
-    //     broadcaster: 'pusher',
-    //     key: '57206333aea283adecc8',
-    //     cluster: 'ap1',
-    //     forceTLS: true,
-    // });
+     const echo = new Echo({
+        broadcaster: 'pusher',
+        key: '57206333aea283adecc8',
+        cluster: 'ap1',
+        forceTLS: true,
+    });
 
-        const echo = new Echo({
-            broadcaster: 'reverb',
-            key: import.meta.env.VITE_REVERB_APP_KEY,
-            wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
-            wsPort: Number(import.meta.env.VITE_REVERB_PORT) || 6001,
-            wssPort: Number(import.meta.env.VITE_REVERB_PORT) || 6001,
-            forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
-            enabledTransports: import.meta.env.VITE_REVERB_SCHEME === 'https' ? ['wss'] : ['ws'],
-          });
+        // const echo = new Echo({
+        //     broadcaster: 'reverb',
+        //     key: import.meta.env.VITE_REVERB_APP_KEY,
+        //     wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
+        //     wsPort: Number(import.meta.env.VITE_REVERB_PORT) || 6001,
+        //     wssPort: Number(import.meta.env.VITE_REVERB_PORT) || 6001,
+        //     forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+        //     enabledTransports: import.meta.env.VITE_REVERB_SCHEME === 'https' ? ['wss'] : ['ws'],
+        //   });
     
 
     // const echo = new Echo({
